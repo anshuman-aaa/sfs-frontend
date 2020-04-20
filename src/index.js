bea1721eba46ddbaf23fba8148ae0403a1218483
meher <meherc99@iitkgp.ac.in>
2020-04-20 22:01:17 +0530

    removed template

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
 import React from "react";
+import { ApolloProvider } from 'react-apollo';
 import ReactDOM from "react-dom";
-import App from "./App/Main/App";
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import { Provider } from "unstated";
-import * as serviceWorker from "./serviceWorker";
+import App from "./App/Main/App";
 import { linkedStores } from "./Helpers/Stores/linkedStores";
+import * as serviceWorker from "./serviceWorker";
+
+
 
 const stores = linkedStores();
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <Provider inject={stores}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <Provider inject={stores}>
+        <App />
+      </Provider>
+    </ApolloProvider>
+  </BrowserRouter>,
+
   document.getElementById("root")
 );
 
