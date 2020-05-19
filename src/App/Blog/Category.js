371da0db8e4081e091fddcfecf53a18dfe11d46c
Anshuman Singh <anshumansinghaaa@gmail.com>
2020-05-19 12:22:52 +0530

    category and blog page contents changed

--- src/App/Blog/Category.js
+++ src/App/Blog/Category.js
@@ -2,13 +2,20 @@ import gql from "graphql-tag";
 import React from "react";
 import { Query } from "react-apollo";
 import { Link } from "react-router-dom";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import ListItemText from "@material-ui/core/ListItemText";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
+import { red } from "@material-ui/core/colors";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Grid from "@material-ui/core/Grid";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import { makeStyles } from "@material-ui/core/styles";
 
 const BlogList = gql`
@@ -33,63 +40,99 @@ function CategoryList(props) {
 
         return (
           <div>
-            <List className={classes.root}>
+            <Grid
+              container
+              className={classes.root}
+              spacing={2}
+              direction="row"
+              justify="flex-start"
+              alignItems="flex-start"
+            >
               {data.findBlogbyCat.map((blog, index) => (
-                  <Link to={`/blog/${blog._id}`}>
-                  <ListItem alignItems="flex-start">
-                    <ListItemAvatar>
-                      <Avatar className={classes.large}
-                        alt="Remy Sharp"
-                        src={"https://picsum.photos/200/300"}
-                      />
-                    </ListItemAvatar>
-                    <ListItemText className={classes.mainHeader}
-                      primary= {blog.blogTitle}
-                      secondary={
-                        <React.Fragment>
-                          <Typography
-                            component="span"
-                            variant="body2"
-                            className={classes.inline}
-                            color="textPrimary"
-                          >
-                            {blog.blogTitle}
-                          </Typography>
-                          {
-                            blog.description
+                <Grid item xs={12} sm={6} md={3} style={{ margin: "0 auto" }}>
+                  <Grid container justify="center" spacing={2}>
+                    <Grid key={"blog" + index} item>
+                      <Card className={classes.card}>
+                        <CardHeader
+                          avatar={
+                            <Avatar
+                              aria-label="recipe"
+                              className={classes.avatar}
+                            >
+                              R
+                            </Avatar>
                           }
-                        </React.Fragment>
-                      }
-                    />
-                  </ListItem>
-                  <Divider variant="inset" component="li" />
-                  </Link>
+                          action={
+                            <IconButton aria-label="settings">
+                              <MoreVertIcon />
+                            </IconButton>
+                          }
+                          title={blog.blogTitle}
+                          subheader={blog.blogTitle}
+                        />
+                        <Link to={`/blog/${blog._id}`}>
+                          <CardMedia
+                            className={classes.media}
+                            image={"https://picsum.photos/200/300"}
+                            title="Paella dish"
+                          />
+                        </Link>
+                        <CardContent>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                          >
+                            This impressive paella is a perfect party dish and a
+                            fun meal to cook together with your guests. Add 1
+                            cup of frozen peas along with the mussels, if you
+                            like.
+                          </Typography>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                          <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                          </IconButton>
+                          <IconButton aria-label="share">
+                            <ShareIcon />
+                          </IconButton>
+                          
+                          <IconButton
+                            style={{"marginLeft": "auto"}}
+                            aria-label="show more"
+                          >
+                            <Link to={`/blog/${blog._id}`}>
+                            <ArrowForwardIcon />
+                            </Link>
+                          </IconButton>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                  </Grid>
+                </Grid>
               ))}
-            </List>
+            </Grid>
           </div>
         );
       }}
     </Query>
   );
 }
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    margin: "0 auto",
-    width: "100%",
-    maxWidth: "50vw",
-    backgroundColor: theme.palette.background.paper
+    flexGrow: 1,
+    padding: theme.spacing(2),
   },
-  inline: {
-    display: "inline"
+  card: {
+    maxWidth: 345,
   },
-  mainHeader: {
-    fontSize: "3em",
-    paddingLeft: "1em"
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
   },
-  large: {
-    width: theme.spacing(9),
-    height: theme.spacing(9)
-  }
+  avatar: {
+    backgroundColor: red[500],
+  },
 }));
 
 export default CategoryList;
--- src/App/Blog/Component/MainFeaturedPost.js
+++ src/App/Blog/Component/MainFeaturedPost.js
@@ -35,12 +35,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function MainFeaturedPost(props) {
+export default function MainFeaturedPost({post}) {
   const classes = useStyles();
-  const { post } = props;
 
   return (
-    <Paper className={classes.mainFeaturedPost} style={{ backgroundImage: `url(${post.image})` }}>
+    <Paper className={classes.mainFeaturedPost} style={{ backgroundImage: `url(${post.blogHeaderImage})` }}>
       {/* Increase the priority of the hero background image */}
       {<img style={{ display: 'none' }} src={post.image} alt={post.imageText} />}
       <div className={classes.overlay} />
@@ -48,10 +47,10 @@ export default function MainFeaturedPost(props) {
         <Grid item md={6}>
           <div className={classes.mainFeaturedPostContent}>
             <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-              {post.title}
+              {post.blogTitle}
             </Typography>
             <Typography variant="h5" color="inherit" paragraph>
-              {post.description}
+              {post.blogTitle}
             </Typography>
             <Link variant="subtitle1" href="#">
               {post.linkText}
